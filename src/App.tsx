a20442daea3f8d81d1482f91871cf225ee58bd90
nousantx <nousantx@gmail.com>
2024-10-29 08:07:29 +0700

    feat: remove unused module

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useLayoutEffect } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { styler } from './styles'
